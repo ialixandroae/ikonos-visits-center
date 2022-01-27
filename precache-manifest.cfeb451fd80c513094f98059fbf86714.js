7dbfd55858342734d756e834d1c50ecaaa0927c2
Ionut Alixandroae <ialixandroae@yahoo.com>
2022-01-27 10:41:41 +0100

    Updates

---
+++ precache-manifest.cfeb451fd80c513094f98059fbf86714.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba8094d95eac6403bf2a1b5ff9ebe0f",
+    "url": "/3d-weather-app/index.html"
+  },
+  {
+    "revision": "314146dde3eef8a19516",
+    "url": "/3d-weather-app/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "7b58853424f717f9faa8",
+    "url": "/3d-weather-app/static/css/main.919ebf7d.chunk.css"
+  },
+  {
+    "revision": "314146dde3eef8a19516",
+    "url": "/3d-weather-app/static/js/2.d2062eb9.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/3d-weather-app/static/js/2.d2062eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b58853424f717f9faa8",
+    "url": "/3d-weather-app/static/js/main.580585a2.chunk.js"
+  },
+  {
+    "revision": "25cfe2b67a74c5d039c1",
+    "url": "/3d-weather-app/static/js/runtime-main.38a46445.js"
+  }
+]);
